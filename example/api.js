cec93342ccfc637cbae63cd039c799f6f7c0d315
Catalin Negru <catalinalexandrunegru@gmail.com>
2020-02-19 13:45:14 -0500

    updated example

--- example/api.js
+++ example/api.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const openExpress = require('../app');
 
+// Define all your operations here
 const api = {
   alive: async (req, res) => {
     res.json({
@@ -9,6 +10,7 @@ const api = {
   },
 };
 
+// BearerAuth Middleware
 const BearerAuth = async (req, res, next) => {
   try {
     const { authorization } = req.headers;
@@ -20,6 +22,7 @@ const BearerAuth = async (req, res, next) => {
   }
 };
 
+// Define all configs
 const config = {
   routerPath: path.join(__dirname, './router.yaml'),
   operations: api,
@@ -33,6 +36,7 @@ const config = {
   },
 };
 
+// Call open-express with config
 openExpress(config)
   .then((app) => {
     app.listen(3000);
